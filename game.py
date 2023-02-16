3b6d0644bf03364dc205a182af5b01c238d4d6cc
keithkahura <kamaukeith391@gmail.com>
2023-02-16 10:28:36 +0300

    finished the game

--- game.py
+++ game.py
@@ -7,8 +7,21 @@ print ("hello there"+ username+"!")
 count=0
 while count<5:
     num=eval(input("enter number:"))
-number = randint(10,50)
-
+    count+= 1
+    rannumber = randint(10,50)
+    if num < rannumber:
+        print("num is too low")
+    elif num > rannumber:
+        print("num is too big")
+    elif num==rannumber:
+        print("winner!!!!!!")
+        break
+print("game over!!!")
+if num==rannumber:
+    print("you win")
+else:
+    print("game over!the correct number is:") 
+    print (num)  
 
 #2 generate a random number
 #3 usinga a while loop:
